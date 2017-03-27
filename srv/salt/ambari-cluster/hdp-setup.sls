39bafbd5a963cff43556db320ec24311fa23c6dd
fengshuo <fengshuo1@niwodai.net>
2017-03-27 18:17:32 +0800

    Modify rebuild_topic.sh for add bash prefix Modify all components auto restart

--- srv/salt/ambari-cluster/hdp-setup.sls
+++ srv/salt/ambari-cluster/hdp-setup.sls
@@ -2,4 +2,3 @@ hdp-setup:
   cmd.run:
     - cwd: /srv/setup
     - name: "sleep 30 && python AmbariRequest.py -f hdp_setup"
-    - onlyif: test 1 -eq `curl -I -H "X-Requested-By:ambari"  -X GET -u admin:admin http://ambari-server1:8080|grep "200 OK"|wc -l`
--- srv/setup/rebuild_topic.sh
+++ srv/setup/rebuild_topic.sh
@@ -1,70 +1,63 @@
 #!/usr/bin/env/ bash
 echo "begin delete topic..."
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic CreateDataSetJobRequestTopic
+cd /usr/hdp/2.5.3.0-37/kafka/bin/
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic CreateDataSetJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic CreateDataSetJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic CreateDataSetJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataInsightJobRequestTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataInsightJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataInsightJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataInsightJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataPreprocessingJobRequestTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataPreprocessingJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataPreprocessingJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic DataPreprocessingJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic FeatureProcessingJobRequestTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic FeatureProcessingJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic FeatureProcessingJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic FeatureProcessingJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic TrainingJobRequestTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic TrainingJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic TrainingJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic TrainingJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic ScoringJobRequestTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic ScoringJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic ScoringJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic ScoringJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --delete --topic ModelRegisterTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --delete --topic ModelRegisterTopic
 sleep 1
 echo "delete topic finished"
 echo "list topic..."
-kafka-topics.sh --zookeeper localhost:2181 --list
+bash kafka-topics.sh --zookeeper localhost:2181 --list
 
 echo "create topic..."
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic CreateDataSetJobRequestT
-opic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic CreateDataSetJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic CreateDataSetJobResponse
-Topic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic CreateDataSetJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataInsightJobRequestTop
-ic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataInsightJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataInsightJobResponseTo
-pic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataInsightJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataPreprocessingJobRequ
-estTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataPreprocessingJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataPreprocessingJobResp
-onseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic DataPreprocessingJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic FeatureProcessingJobRequ
-estTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic FeatureProcessingJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic FeatureProcessingJobResp
-onseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic FeatureProcessingJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic TrainingJobRequestTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic TrainingJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic TrainingJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic TrainingJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic ScoringJobRequestTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic ScoringJobRequestTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic ScoringJobResponseTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic ScoringJobResponseTopic
 sleep 1
-kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic ModelRegisterTopic
+bash kafka-topics.sh --zookeeper localhost:2181 --create --partitions 3 --replication 2 --topic ModelRegisterTopic
 sleep 1
 echo "create topic finished"
 echo "list topic..."
-kafka-topics.sh --zookeeper localhost:2181 --list
+bash kafka-topics.sh --zookeeper localhost:2181 --list
